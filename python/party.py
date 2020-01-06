a5be039373d585f48855f8b42defd1f06e0ef65f
suzuki-hoge <user.ryo@gmail.com>
2020-01-06 22:21:55 +0900

    copy to python.

---
+++ python/party.py
@@ -0,0 +1,90 @@
+import payment
+
+class Party:
+    @staticmethod
+    def plan(partyName, partyHoldAt, participantName, paymentSection, billingAmount, adjustingUnitAmount):
+        return Party(partyName, partyHoldAt, [Participant(participantName, 'Sec', paymentSection)], billingAmount, adjustingUnitAmount)
+
+
+    def __init__(self, partyName, partyHoldAt, participants, billingAmount, adjustingUnitAmount):
+        self.partyName = partyName
+        self.partyHoldAt = partyHoldAt
+        self.participants = participants
+        self.billingAmount = billingAmount
+        self.adjustingUnitAmount = adjustingUnitAmount
+
+    def add(self, participant):
+        self.participants = self.participants + [participant]
+        return self
+
+    def remove(self, participantName):
+        self.participants = filter(lambda p: p.participantName != participantName, self.participants)
+        return self
+
+    def debug(self):
+        print self.partyName
+        print self.partyHoldAt
+        [p.debug('  ') for p in self.participants]
+        print self.billingAmount
+        print self.adjustingUnitAmount
+
+    def change(self, adjustingUnitAmount):
+        self.adjustingUnitAmount = adjustingUnitAmount
+        return self
+
+    def demand(self):
+        def bind(units, fractionAmount, p):
+            paymentAmount = filter(lambda (paymentSection, paymentAmount): paymentSection == p.paymentSection, units)[0][1]
+            if p.participantType == 'Sec':
+                return (p.participantName, paymentAmount + fractionAmount)
+            else:
+                return (p.participantName, paymentAmount)
+
+        (units, fractionAmount) = payment.calculate(self.billingAmount, (map(lambda p: p.paymentSection, self.participants)), self.adjustingUnitAmount)
+        return map(lambda p: bind(units, fractionAmount, p), self.participants)
+
+
+class Participant:
+    def __init__(self, participantName, participantType, paymentSection):
+        self.participantName = participantName
+        self.participantType = participantType
+        self.paymentSection = paymentSection
+
+    def debug(self, indent):
+        print indent + self.participantName
+        print indent + self.participantType
+        print indent + self.paymentSection
+
+
+def add(party, participant):
+    return party.add(participant)
+
+
+# party = Party.plan('new-year', '2019/01/01', 'p1', 'M', 60000, 1000)
+# party.debug()
+
+# added = party.add(Participant('p2', 'NotSec', 'N'))
+# added.debug()
+
+# removed = added.remove('p2')
+# removed.debug()
+
+# changed = removed.change(500)
+# changed.debug()
+
+
+party = Party(
+    'new-year',
+    '2019/01/01',
+    [
+        Participant('p1', 'Sec',    'M'),
+        Participant('p2', 'NotSec', 'M'),
+        Participant('p3', 'NotSec', 'M'),
+        Participant('p4', 'NotSec', 'N'),
+        Participant('p5', 'NotSec', 'L'),
+        Participant('p6', 'NotSec', 'L')
+    ],
+    65000,
+    500
+)
+print party.demand()
---
+++ python/payment.py
@@ -0,0 +1,75 @@
+def calculate(billingAmount, paymentSections, adjustingUnitAmount):
+    divideNumber = len(paymentSections)
+    (sharingAmount, fractionAmount) = divide(billingAmount, divideNumber, adjustingUnitAmount)
+    units = adjustByUnit(paymentSections)
+    return (sharing(sharingAmount, adjustingUnitAmount, divideNumber, units), fractionAmount)
+
+
+def divide(billingAmount, divideNumber, adjustingUnitAmount):
+    sharing = billingAmount / divideNumber / adjustingUnitAmount * divideNumber * adjustingUnitAmount
+    if sharing == 0:
+        raise BaseException('unsharable adjusting unit amount')
+    else:
+        return (sharing, billingAmount - sharing)
+
+
+def adjustByUnit(paymentSections):
+    def get_lcm(x, y):
+        import fractions
+        return (x * y) // fractions.gcd(x, y)
+
+    def morePresent():
+        def point(paymentSection):
+            return {'M': 1, 'N': -1, 'L': -2}[paymentSection]
+        
+        def adjust(lcm, plus, minus, paymentSection):
+            return {'M': ('M', lcm / plus), 'N': ('N', lcm / minus), 'L': ('L', lcm / minus * 2)}[paymentSection]
+
+        return (point, adjust)
+
+    def moreMissing():
+        def point(paymentSection):
+            return {'N': 1, 'L': -1}[paymentSection]
+        
+        def adjust(lcm, plus, minus, paymentSection):
+            return {'N': ('N', lcm / plus), 'L': ('L', lcm / minus)}[paymentSection]
+
+        return (point, adjust)
+
+    (pointer, adjuster) = morePresent() if 'M' in paymentSections else moreMissing()
+
+    plus  = sum(filter(lambda n: n > 0, map(pointer, paymentSections)))
+    minus = sum(filter(lambda n: n < 0, map(pointer, paymentSections)))
+    lcm = get_lcm(plus, minus)
+
+    return [adjuster(lcm, plus, minus, paymentSection) for paymentSection in list(set(paymentSections))]
+
+
+def sharing(sharingAmount, adjustingUnitAmount, divideNumber, units):
+    def fix(ave, adjustingUnitAmount, adjustingUnitCount):
+        return adjustingUnitAmount * adjustingUnitCount + ave
+
+    ave = sharingAmount / divideNumber
+    fixed = [(paymentSection, fix(ave, adjustingUnitAmount, adjustingUnitCount)) for (paymentSection, adjustingUnitCount) in units]
+
+    if all([paymentAmount >= 0 for (paymentSection, paymentAmount) in fixed]):
+        return fixed
+    else:
+        raise BaseException('too large adjusting unit amount')
+
+
+# print divide(1000, 3, 100) # 900, 100
+# print divide(1199, 3, 100) # 900, 299
+# print divide(1200, 3, 500) # unsharable adjusting unit amount
+
+# print adjustByUnit(['M', 'M', 'M', 'N', 'L', 'L']) #  5, -3, -6
+# print adjustByUnit(['M', 'N'])                     #  1, -1
+# print adjustByUnit(['N', 'L'])                     #  1, -1
+
+# print sharing(  900,  100, 3, [('M', 3), ('N', -1), ('L', -2)]) #   600,  200,  100
+# print sharing(60000, 1000, 6, [('M', 5), ('N', -3), ('L', -6)]) # 15000, 7000, 4000
+# print sharing( 6000, 1000, 6, [('M', 5), ('N', -3), ('L', -6)]) # too large adjusting unit amount
+
+# print calculate(  900, ['M', 'N', 'L'], 100) # 600, 200, 100,  0
+# print calculate(  990, ['M', 'N', 'L'], 100) # 600, 200, 100, 90
+# print calculate(65000, ['M', 'M', 'M', 'N', 'L', 'L'], 500) # 13000, 9000, 7500
